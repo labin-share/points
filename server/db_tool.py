8f4e67c78991736704c6a0ddad95401c9f9426a1
vera <874343081@qq.com>
2018-06-10 18:31:00 +0800

    fixed merge error

---
+++ server/db_tool.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient, ASCENDING
+
+client = MongoClient('localhost', 27017)
+
+points_db = None
+
+
+def connect_db():
+    global points_db
+    points_db = client.points
+
+
+def search(name, phone):
+    query = {}
+    if (name):
+        query['name'] = name
+    if (phone):
+        query['phone'] = phone
+    return points_db.customer.find(query).sort("name", ASCENDING)
+
+
+def create(data):
+    points_db.customer.insert(data)
+    return None
+
+
+def update(query, data):
+    points_db.customer.update(query,{"$set":data})
+    return None
+
+
+if __name__ == '__main__':
+    connect_db()
--- server/main.py
+++ server/main.py
@@ -1,71 +1,87 @@
-from flask import Flask
-from flask import request, jsonify, Response, render_template
 import json
+
+from bson import json_util
+from flask import Flask
+from flask import request, render_template, json
+
+import db_tool
+
+db_tool.connect_db()
 app = Flask(__name__)
 
-users = []
-
-def create_user(name, phone,score):
-	return {"name":name, "phone":phone, "score":score}
 
 @app.route('/')
 def hello():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/test')
-def test():
-	return 'hello world'
-	
-@app.route('/save')
+
+@app.route('/save', methods=['POST'])
 def save():
-	name = request.args.get('name')
-	phone = request.args.get('phone')
-	score = request.args.get('score',0)
-	print(score)
-	global users
-	users.append(create_user(name,phone,score))
-	return jsonify(users)
-	
+    data = json.loads(request.data)
+    db_tool.create(data)
+    name = json.loads(request.data).get('name')
+    phone = json.loads(request.data).get('phone')
+    next_user_list = [{'name':name, 'phone':phone}]
+    previous_info = json.loads(request.data).get('previous')
+    previous_phone = previous_info.get('phone')
+    previous_name = previous_info.get('name')
+    if (previous_info):
+        previous_user = db_tool.search(name=previous_name, phone=previous_phone)
+        user_info = convert_to_list(previous_user)[0]
+        if (not user_info['next']):
+            user_info['next'] = []
+        user_info['next'].append(next_user_list)
+        db_tool.update({"name": previous_name}, user_info)
+    return "success"
+
+
 @app.route('/users')
 def search():
-	global users
-	name = request.args.get('name')
-	if(name is None):
-		return jsonify(users)
-	result = findUser(name)	
-	return jsonify(result)
-	
-	
-@app.route('/increase')
+    name = request.args.get('name')
+    phone = request.args.get('phone')
+    result = db_tool.search(name, phone)
+    return json_util.dumps(result)
+
+
+@app.route('/score/increase', methods=['PUT'])
 def increase():
-	name = request.args.get('name')
-	increaseValue=request.args.get('score')
-	result = findUser(name)	
-	defaultScore=int(result['score'])
-	defaultScore+= int(increaseValue)
-	result['score']=defaultScore
-	return str(result)
+    result = get_user_info()
+    user_info = convert_to_list(result)
+    if (user_info):
+        user_info = user_info[0]
+        score = json.loads(request.data).get('score')
+        user_info['score'] += int(score)
+        db_tool.update({"name": user_info['name'], "phone": user_info['phone']}, user_info)
+        return "Success"
+    return "Fail"
 
-@app.route('/decrease')
+
+@app.route('/score/decrease', methods=['PUT'])
 def decrease():
-	name = request.args.get('name')
-	increaseValue=request.args.get('score')
-	result = findUser(name)	
-	defaultScore=int(result['score'])
-	defaultScore-= int(increaseValue)
-	result['score']=defaultScore
-	return str(result)
+    result = get_user_info()
+    user_info = convert_to_list(result)
+    if (user_info):
+        user_info = user_info[0]
+        score = json.loads(request.data).get('score')
+        user_info['score'] -= int(score)
+        db_tool.update({"name": user_info['name'], "phone": user_info['phone']}, user_info)
+        return "success"
+    return "Fail"
 
-	
-def findUser(name):
-	result = []
-	for user in users:
-		if user['name'] == name:
-			result = user 
-			break
-	return result
-	
+
+def get_user_info():
+    phone = json.loads(request.data).get('phone')
+    name = json.loads(request.data).get('name')
+    result = db_tool.search(phone=phone, name=name)
+    return result
+
+
+def convert_to_list(result):
+    list = []
+    for item in result:
+        list.append(item)
+    return list
+
+
 if __name__ == '__main__':
-	app.run()
-	
-	
+    app.run(host='192.168.31.207', port=5000, debug=True)
